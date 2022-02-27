1f4b5af1b66a8434831f5bd908bf2d4d1fd9fb7d
Seongheon Sim <sh.sim@monthlything.com>
2022-02-28 02:00:37 +0900

    [Add : #1] Project Setting
    - 멀티 모듈 프로젝트 세팅

---
+++ core/src/test/java/com/project/siren/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.siren.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
